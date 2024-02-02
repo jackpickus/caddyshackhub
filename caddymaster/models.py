c532f6b33e9c1dc83bc5d51c4c4bf81f00039bde
Jack Pickus <jpick329@gmail.com>
2024-02-01 22:59:22 -0600

    init caddymaster and caddyshack models

--- caddymaster/models.py
+++ caddymaster/models.py
@@ -1,3 +1,26 @@
-from django.db import models
+import datetime
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+class CaddyMaster(models.Model):
+
+    user = models.OneToOneField(User, on_delete=models.SET_NULL)
+    email_validated = models.BooleanField(default=False)
+    change_email = models.EmailField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+class CaddyShack(models.Model):
+    caddy_shack_title = models.CharField(max_length=100)
+    date = models.DateField(default=datetime.date.today)
+
+    caddy_master = models.ForeignKey(CaddyMaster, on_delete=models.CASCADE)
+    golfer_groups = models.JSONField(null=True)
+
+    class Meta:
+        ordering = ["-date"]
+
+    def __str__(self):
+        return self.caddy_shack_title
