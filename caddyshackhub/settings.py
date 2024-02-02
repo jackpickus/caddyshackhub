89ec09a2ba8a6ae59bf65b4c154eccfe14d5c935
Jack Pickus <jpick329@gmail.com>
2024-02-01 22:42:59 -0600

    add caddymaster app to settings

--- caddyshackhub/settings.py
+++ caddyshackhub/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'loopers.apps.LoopersConfig',
+    'caddymaster.apps.CaddymasterConfig',
 ]
 
 MIDDLEWARE = [
