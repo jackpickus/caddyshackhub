38bc3addd879f56017b688436de787d7b833f299
Jack Pickus <jpick329@gmail.com>
2024-02-13 18:54:37 -0600

    make loopers base url

--- caddyshackhub/urls.py
+++ caddyshackhub/urls.py
@@ -19,6 +19,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('loopers/', include('loopers.urls')),
+    path('', include('loopers.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
