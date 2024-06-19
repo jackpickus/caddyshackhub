a4d7061b318f6ead01f4525b63e56b6166cb9a56
Jack Pickus <jpick329@gmail.com>
2024-06-19 17:13:14 -0500

    update models

--- caddymaster/models.py
+++ caddymaster/models.py
@@ -1,6 +1,8 @@
 import datetime
 
 from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 
 from loopers.models import Caddy
 
@@ -11,24 +13,23 @@ class CaddyMaster(Caddy):
 
 class CaddyShack(models.Model):
     caddy_shack_title = models.CharField(max_length=100)
-    date = models.DateField(default=datetime.date.today)
-
     caddy_master = models.ForeignKey(CaddyMaster, on_delete=models.CASCADE)
-    caddys = models.ManyToManyField(Caddy, blank=True, related_name="caddys_in_shack")
-    golfer_groups = models.JSONField(null=True)
-
-    class Meta:
-        ordering = ["-date"]
+    caddys = models.ManyToManyField(User, related_name="caddys_in_shack")
 
     def __str__(self):
         return self.caddy_shack_title
 
+
+def get_sentinel_user():
+    return get_user_model().objects.get_or_create(username="deleted_caddy")[0]
+
 class TeeTime(models.Model):
     golfers = models.JSONField(null=False)
-    caddy = models.CharField(max_length=100)
+    caddy = models.ForeignKey(User, on_delete=models.SET(get_sentinel_user))
     time = models.DateField(default=datetime.date.today)
     position = models.IntegerField(null=True, blank=True)
     flight = models.CharField(max_length=4, blank=True)
+    caddy_shack = models.ForeignKey(CaddyShack, on_delete=models.CASCADE)
 
     def __str__(self) -> str:
         golfers_and_time = self.golfers + self.time.strftime
