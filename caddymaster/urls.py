ffb513beee806e1e14bcedc6305505918734d4c6
Jack Pickus <jpick329@gmail.com>
2024-06-26 15:18:30 -0500

    init new teetime view

--- caddymaster/urls.py
+++ caddymaster/urls.py
@@ -5,4 +5,6 @@ from . import views
 app_name = 'caddymaster'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
+    path('teetime/new', views.new_teetime, name='new_teetime'),
+    # path('teetime/<int:pk>', views.TeeTimeDetailView.as_view(), name='teetime-detail'),
 ]
--- caddymaster/views.py
+++ caddymaster/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
 from django.views import generic
+from django.contrib.auth.decorators import permission_required
 
 from .models import TeeTime
+from .forms import AssignLoopForm
 
 class IndexView(generic.ListView):
     model = TeeTime
     template_name = "caddymaster/index.html"
-    # context_object_name = "all_tee_times"
+
+@permission_required("caddymaster.can_create_teetimes")
+def new_teetime(request):
+    if request.method == "POST":
+        f = AssignLoopForm(request.POST)
+        if f.is_valid():
+            print("Save Loop and Assign to caddy")
+    else:
+        f = AssignLoopForm()
+
+    return render(request, "caddymaster/assign_loop.html", {"form": f})
 
