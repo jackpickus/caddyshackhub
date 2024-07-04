998d630507e12e2dff983df2d77c69fe8d073edf
Jack Pickus <jpick329@gmail.com>
2024-07-04 11:09:28 -0500

    url to individual teetime

--- caddymaster/urls.py
+++ caddymaster/urls.py
@@ -6,5 +6,5 @@ app_name = 'caddymaster'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('teetime/new', views.new_teetime, name='new_teetime'),
-    # path('teetime/<int:pk>', views.TeeTimeDetailView.as_view(), name='teetime-detail'),
+    path('teetime/<int:pk>', views.TeeTimeDetailView.as_view(), name='teetime-detail'),
 ]
