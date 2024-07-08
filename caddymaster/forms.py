fef7d29cc8fcd24bdde830ed81dafeac34ed6c9a
Jack Pickus <jpick329@gmail.com>
2024-07-08 17:16:37 -0500

    post new teetime

--- caddymaster/forms.py
+++ caddymaster/forms.py
@@ -5,4 +5,14 @@ from .models import TeeTime
 class NewTeeTimeForm(forms.ModelForm):
     class Meta:
         model = TeeTime
-        exclude = ["caddy_shack"]
+        exclude = ("caddy_shack",)
+
+    def __init__(self, *args, **kwargs):
+        initial_arguments = kwargs.get('initial', None)
+        if initial_arguments:
+            caddy = initial_arguments.get("caddy", None)
+        elif args:
+            caddy = args[0].__getitem__('caddy')
+
+        super().__init__(*args, **kwargs)
+        self.fields["caddy"].choices = caddy
--- caddymaster/views.py
+++ caddymaster/views.py
@@ -12,21 +12,28 @@ class IndexView(generic.ListView):
 
 @permission_required("caddymaster.can_create_teetimes", raise_exception=True)
 def new_teetime(request):
+    cm = CaddyMaster.objects.get(user=request.user.id)
+    caddyshack = CaddyShack.objects.get(caddy_master=cm)
     if request.method == "POST":
         f = NewTeeTimeForm(request.POST)
         if f.is_valid():
             teetime = f.save(commit=False)
-            cm = CaddyMaster.objects.get(user=request.user.id)
-            caddyshack = CaddyShack.objects.get(caddy_master=cm)
             teetime.caddy_shack = caddyshack
             teetime.save()
             messages.success(request, "TeeTime created!")
             return render(request, "caddymaster/index.html")
     else:
-        cm = CaddyMaster.objects.get(user=request.user.id)
-        print(cm.user.get_all_permissions())
-        print("Caddymaster permissions")
-        f = NewTeeTimeForm()
+        # get caddies and prefill dropdown list in form
+        caddies_in_shack = caddyshack.caddys.all() # this is a list of User objects
+        caddy_names = []
+        for caddy in caddies_in_shack:
+            the_caddy_obj = []
+            the_caddy_obj.append(caddy.id)
+            the_caddy_obj.append(caddy.username) 
+            caddy_names.append(tuple(the_caddy_obj)) # the 'caddy' object is really a User object
+            
+
+        f = NewTeeTimeForm(initial={"caddy": caddy_names})
 
     return render(request, "caddymaster/new_teetime.html", {"form": f})
 
